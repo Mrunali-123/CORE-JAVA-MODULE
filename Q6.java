907a15f3622e5e7a4d67278730a644e3b3128172
Mrunali-123 <68396804+Mrunali-123@users.noreply.github.com>
2020-09-18 23:25:42 +0530

    Add files via upload

---
+++ Q6.java
@@ -0,0 +1,6 @@
+class Q6{
+        public static void main(String args[]){
+           String s1=args[0];
+           System.out.println("welcome "+s1);
+        }
+}
---
+++ forDemo.java
@@ -0,0 +1,7 @@
+class forDemo{
+         public static void main(String args[]){
+            for(int i=0; i<10; j--){
+             System.out.println(i+"  "+j);
+             }
+      }
+}

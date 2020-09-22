f4c70991cfa1e2c3ed068cdf2ac9f391aaa90213
Mrunali-123 <68396804+Mrunali-123@users.noreply.github.com>
2020-09-23 00:41:34 +0530

    Add files via upload

---
+++ Q18.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class Q18
+{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number");
+		int num = sc.nextInt();
+		boolean isPrime = true;
+		if(num == 0 || num == 1) System.out.println("Not prime number");
+		else{
+			for(int i=2;i<num/2;i++){
+				if(num % i == 0){
+					isPrime = false;
+					break;
+				}
+			}
+			if(isPrime == false) System.out.println(num+" Is not prime");
+		else System.out.println(num+" Is prime");
+		}
+	}
+}
